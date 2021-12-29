33e97e683950a612c0739acf196577018205d520
waterproofpatch <waterproofpatch@gmail.com>
2021-12-29 10:51:42 -0500

    default money

--- src/app/players.ts
+++ src/app/players.ts
@@ -1,10 +1,11 @@
 import { Player } from './player';
 
+export const STARTING_MONEY: number = 1500;
 export const PLAYERS: Player[] = [
-  { img: 'dog', name: 'Dog', money: 1000 },
-  { img: 'ship', name: 'Ship', money: 1000 },
-  { img: 'dino', name: 'Dinosaur', money: 1000 },
-  { img: 'car', name: 'Car', money: 1000 },
+  { img: 'dog', name: 'Dog', money: STARTING_MONEY },
+  { img: 'ship', name: 'Ship', money: STARTING_MONEY },
+  { img: 'dino', name: 'Dinosaur', money: STARTING_MONEY },
+  { img: 'car', name: 'Car', money: STARTING_MONEY },
   { img: 'bank', name: 'Bank', money: 0 },
   { img: 'parking', name: 'Free Parking', money: 0 },
 ];
