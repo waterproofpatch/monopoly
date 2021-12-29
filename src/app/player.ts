95d06433f2436c369d2d4edc1493bebcfcf7e497
waterproofpatch <waterproofpatch@gmail.com>
2021-12-28 21:46:01 -0500

    images

--- src/app/player.ts
+++ src/app/player.ts
@@ -1,5 +1,5 @@
 export interface Player {
-  id: number;
+  img: string;
   name: string;
   money: number;
 }
--- src/app/players.ts
+++ src/app/players.ts
@@ -1,10 +1,10 @@
 import { Player } from './player';
 
 export const PLAYERS: Player[] = [
-  { id: 1, name: 'Dog', money: 1000 },
-  { id: 2, name: 'Ship', money: 1000 },
-  { id: 3, name: 'Dinosaur', money: 1000 },
-  { id: 4, name: 'Car', money: 1000 },
-  { id: 5, name: 'Bank', money: 0 },
-  { id: 6, name: 'Free Parking', money: 0 },
+  { img: 'dog', name: 'Dog', money: 1000 },
+  { img: 'ship', name: 'Ship', money: 1000 },
+  { img: 'dino', name: 'Dinosaur', money: 1000 },
+  { img: 'car', name: 'Car', money: 1000 },
+  { img: 'bank', name: 'Bank', money: 0 },
+  { img: 'parking', name: 'Free Parking', money: 0 },
 ];
