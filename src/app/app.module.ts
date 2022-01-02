7f9a43b020d28070eba7e6fbe4fe7bd3d3202432
waterproofpatch <waterproofpatch@gmail.com>
2022-01-02 18:18:21 -0500

    UI redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgMode
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
-import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { GameBoardComponent } from './game-board/game-board.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlayersComponent,
-    PlayerDetailComponent,
-    GameBoardComponent,
+  declarations: [AppComponent, PlayersComponent, GameBoardComponent],
+  imports: [
+    MatDialogModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
-  imports: [FormsModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,6 +1,23 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { Player, Transaction } from '../player';
 import { LogService } from '../log-service.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+
+export interface DialogData {
+  errorMsg: string;
+}
 
 @Component({
   selector: 'app-players',
@@ -11,19 +28,37 @@ export class PlayersComponent implements OnInit {
   @Input() players?: Player[]; // from game-board
   @Input() transactions?: Transaction[]; // from game-board
   @Output() gameState = new EventEmitter<Transaction>();
-  selectedPlayer?: Player;
-  errorMsg: string = '';
 
-  constructor(private logger: LogService) {}
+  transactionForm = new FormGroup({
+    otherPlayer: new FormControl(''),
+    amount: new FormControl(''),
+  });
 
-  ngOnInit(): void {}
-  ngOnChanges(): void {
-    this.errorMsg = '';
+  constructor(private logger: LogService, private dialog: MatDialog) {}
+
+  openDialog(errorMsg: string): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: { errorMsg: errorMsg },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
+  ngOnInit(): void {}
 
-  onSelect(player: Player): void {
-    this.errorMsg = '';
-    this.selectedPlayer = player;
+  findPlayerByName(name: string): Player | null {
+    if (!this.players) {
+      return null;
+    }
+
+    for (let p of this.players) {
+      if (p.name == name) {
+        return p;
+      }
+    }
+    return null;
   }
 
   handleTransaction(transaction: Transaction): void {
@@ -36,14 +71,16 @@ export class PlayersComponent implements OnInit {
         transaction.amount
     );
 
-    this.errorMsg = '';
-
     // the bank has unlimited money
     if (
       transaction.fromPlayer.name != 'Bank' &&
       transaction.fromPlayer.money < transaction.amount
     ) {
-      this.errorMsg = 'Not enough money!';
+      this.openDialog(
+        'Not enough money. Must raise $' +
+          (transaction.amount - transaction.fromPlayer.money) +
+          '.'
+      );
       return;
     }
 
@@ -51,5 +88,52 @@ export class PlayersComponent implements OnInit {
 
     transaction.fromPlayer.money -= transaction.amount;
     transaction.toPlayer.money += transaction.amount;
+  }
+
+  makePayment(player: Player): void {
+    this.logger.log('Handling transaction');
+    if (!this.players) {
+      this.openDialog('No players available!');
+      return;
+    }
+
+    if (this.transactionForm.controls.otherPlayer.value == player.name) {
+      this.openDialog('Cannot pay yourself!');
+      return;
+    }
+
+    let otherPlayer: Player | null = this.findPlayerByName(
+      this.transactionForm.controls.otherPlayer.value
+    );
+    if (!otherPlayer) {
+      this.openDialog(
+        "No player by name of '" +
+          this.transactionForm.controls.otherPlayer.value +
+          "'"
+      );
+      return;
+    }
+
+    let t: Transaction = {
+      fromPlayer: player,
+      toPlayer: otherPlayer,
+      amount: this.transactionForm.controls.amount.value,
+    };
+    this.handleTransaction(t);
+    this.transactionForm.reset();
+  }
+}
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './dialog.html',
+})
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onOkClick(): void {
+    this.dialogRef.close();
   }
 }
