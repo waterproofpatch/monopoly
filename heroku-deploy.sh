0a06e681eaa24b65478b58bc95c7710a010d291b
waterproofpatch <waterproofpatch@gmail.com>
2022-01-02 19:08:22 -0500

    fix typo

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,4 +1,4 @@
 heroku container:login
 heroku container:push monopoly --app monopoly-banker
-heroku container:release monopoly --app-monopoly-banker
+heroku container:release monopoly --app monopoly-banker
 heroku ps:scale monopoly=1 --app monopoly-banker
