2cb5161ae4ec9931f0436b6f0a7d59ef7cd27a9a
waterproofpatch <waterproofpatch@gmail.com>
2022-01-06 22:31:54 -0500

    rework format

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -2,7 +2,6 @@ import { Input, Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Player, Transaction } from '../types';
 import { DialogService } from '../dialog.service';
-
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
@@ -30,52 +29,6 @@ export class PlayerDetailComponent implements OnInit {
     this.logger.displayPieceSelectDialog(this.player, this.players);
   }
 
-  findPlayerByName(name: string): Player | null {
-    if (!this.players) {
-      return null;
-    }
-
-    for (let p of this.players) {
-      if (p.name == name) {
-        return p;
-      }
-    }
-    return null;
-  }
-  makePayment(): void {
-    this.logger.log('Handling transaction');
-    if (!this.players || !this.player) {
-      this.logger.displayErrorDialog('No players available!');
-      return;
-    }
-
-    if (this.transactionForm.controls.otherPlayer.value == this.player.name) {
-      this.logger.displayErrorDialog('Cannot pay yourself!');
-      return;
-    }
-
-    let otherPlayer: Player | null = this.findPlayerByName(
-      this.transactionForm.controls.otherPlayer.value
-    );
-    if (!otherPlayer) {
-      this.logger.displayErrorDialog(
-        "No player by name of '" +
-          this.transactionForm.controls.otherPlayer.value +
-          "'"
-      );
-      return;
-    }
-
-    let t: Transaction = {
-      timestamp: new Date().toISOString(),
-      fromPlayer: this.player,
-      toPlayer: otherPlayer,
-      amount: this.transactionForm.controls.amount.value,
-    };
-    this.handleTransaction(t);
-    this.transactionForm.reset();
-  }
-
   otherPlayers(): Player[] {
     if (!this.players || !this.player) {
       this.logger.displayErrorDialog('Players is not set!');
@@ -90,34 +43,5 @@ export class PlayerDetailComponent implements OnInit {
       }
     }
     return retPlayers;
-  }
-
-  handleTransaction(transaction: Transaction): void {
-    this.logger.log(
-      'Transaction from ' +
-        transaction.fromPlayer.name +
-        ' to ' +
-        transaction.toPlayer.name +
-        ' in the amount of ' +
-        transaction.amount
-    );
-
-    // the bank has unlimited money
-    if (
-      transaction.fromPlayer.name != 'Bank' &&
-      transaction.fromPlayer.money < transaction.amount
-    ) {
-      this.logger.displayErrorDialog(
-        'Not enough money. Must raise $' +
-          (transaction.amount - transaction.fromPlayer.money) +
-          '.'
-      );
-      return;
-    }
-
-    this.gameState.emit(transaction);
-
-    transaction.fromPlayer.money -= transaction.amount;
-    transaction.toPlayer.money += transaction.amount;
   }
 }
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Player, Transaction } from '../types';
+import { DialogService } from '../dialog.service';
 
 @Component({
   selector: 'app-players',
@@ -11,12 +13,93 @@ export class PlayersComponent implements OnInit {
   @Input() transactions?: Transaction[]; // from game-board
   @Output() gameState = new EventEmitter<Transaction>();
 
-  constructor() {}
+  transactionForm = new FormGroup({
+    fromPlayerName: new FormControl(''),
+    toPlayerName: new FormControl(''),
+    amount: new FormControl(''),
+  });
+
+  constructor(private logger: DialogService) {}
 
   ngOnInit(): void {}
+  handleTransaction(transaction: Transaction): void {
+    this.logger.log(
+      'Transaction from ' +
+        transaction.fromPlayer.name +
+        ' to ' +
+        transaction.toPlayer.name +
+        ' in the amount of ' +
+        transaction.amount
+    );
+
+    // the bank has unlimited money
+    if (
+      transaction.fromPlayer.name != 'Bank' &&
+      transaction.fromPlayer.money < transaction.amount
+    ) {
+      this.logger.displayErrorDialog(
+        'Not enough money. Must raise $' +
+          (transaction.amount - transaction.fromPlayer.money) +
+          '.'
+      );
+      return;
+    }
+
+    this.gameState.emit(transaction);
+
+    transaction.fromPlayer.money -= transaction.amount;
+    transaction.toPlayer.money += transaction.amount;
+  }
+  makePayment(): void {
+    this.logger.log('Handling transaction');
+    if (!this.players) {
+      this.logger.displayErrorDialog('No players available!');
+      return;
+    }
+
+    if (
+      this.transactionForm.controls.fromPlayerName.value ==
+      this.transactionForm.controls.toPlayerName.value
+    ) {
+      this.logger.displayErrorDialog('Cannot pay yourself!');
+      return;
+    }
+
+    let toPlayer: Player | null = this.findPlayerByName(
+      this.transactionForm.controls.toPlayerName.value
+    );
+    let fromPlayer: Player | null = this.findPlayerByName(
+      this.transactionForm.controls.fromPlayerName.value
+    );
+    if (!toPlayer || !fromPlayer) {
+      this.logger.displayErrorDialog('Unable to find to and from player!');
+      return;
+    }
+
+    let t: Transaction = {
+      timestamp: new Date().toISOString(),
+      fromPlayer: fromPlayer,
+      toPlayer: toPlayer,
+      amount: this.transactionForm.controls.amount.value,
+    };
+    this.handleTransaction(t);
+    this.transactionForm.reset();
+  }
 
   updateGameState(t: Transaction): void {
     // forward the event up to the game board
     this.gameState.emit(t);
+  }
+  findPlayerByName(name: string): Player | null {
+    if (!this.players) {
+      return null;
+    }
+
+    for (let p of this.players) {
+      if (p.name == name) {
+        return p;
+      }
+    }
+    return null;
   }
 }
