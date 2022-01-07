d05d462ad174450539ee542bd1ee1c40896a224a
waterproofpatch <waterproofpatch@gmail.com>
2022-01-07 01:04:42 -0500

    update images

--- src/app/players.ts
+++ src/app/players.ts
@@ -6,6 +6,9 @@ export const PLAYERS: Player[] = [
   { img: 'ship', name: 'Ship', money: STARTING_MONEY, human: true },
   { img: 'dino', name: 'Dinosaur', money: STARTING_MONEY, human: true },
   { img: 'car', name: 'Car', money: STARTING_MONEY, human: true },
+  { img: 'cat', name: 'Cat', money: STARTING_MONEY, human: true },
+  { img: 'duck', name: 'Duck', money: STARTING_MONEY, human: true },
+  { img: 'penguin', name: 'Penguin', money: STARTING_MONEY, human: true },
   { img: 'bank', name: 'Bank', money: 0, human: false },
   { img: 'parking', name: 'Free Parking', money: 0, human: false },
 ];
