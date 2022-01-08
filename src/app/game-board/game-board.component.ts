d37bcb35286b2221e0a5407f3f3f3437bed5590a
waterproofpatch <waterproofpatch@gmail.com>
2022-01-08 12:55:37 -0500

    refactor dirs

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Player, Transaction } from '../types';
 import { DialogService } from '../dialog-service/dialog.service';
 import { TransactionService } from '../transaction-service/transaction.service';
-import { PlayerService } from '../player.service';
+import { PlayerService } from '../player-service/player.service';
 
 @Component({
   selector: 'app-game-board',
--- src/app/player-service/player.service.ts
+++ src/app/player-service/player.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Player } from './types';
+import { Player } from '../types';
 
 @Injectable({
   providedIn: 'root',
