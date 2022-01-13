e6c42fc22c37990e7171bc640ffab29553ab6237
waterproofpatch <waterproofpatch@gmail.com>
2022-01-13 00:18:36 -0500

    wildcard

--- backend/views.go
+++ backend/views.go
@@ -17,9 +17,8 @@ func players(w http.ResponseWriter, r *http.Request) {
 	db := getDb()
 	db.Find(&players)
 
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Access-Control-Allow-Origin")
-	// w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	json.NewEncoder(w).Encode(players)
 }
