4c11edd1c95cb9416e32f6632044ab0d284cbb2d
waterproofpatch <waterproofpatch@gmail.com>
2022-01-14 00:33:31 -0500

    no default players

--- frontend/src/app/player-service/player.service.ts
+++ frontend/src/app/player-service/player.service.ts
@@ -11,66 +11,6 @@ import { environment } from '../../environments/environment'; // Change this to 
 })
 export class PlayerService {
   startingMoney: number = 1500;
-  players: Player[] = [
-    {
-      img: 'dog',
-      name: 'Dog',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    {
-      img: 'ship',
-      name: 'Ship',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    {
-      img: 'dino',
-      name: 'Dinosaur',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    {
-      img: 'car',
-      name: 'Car',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    {
-      img: 'cat',
-      name: 'Cat',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    {
-      img: 'duck',
-      name: 'Duck',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    {
-      img: 'penguin',
-      name: 'Penguin',
-      money: this.startingMoney,
-      human: true,
-      inGame: true,
-    },
-    { img: 'bank', name: 'Bank', money: 0, human: false, inGame: true },
-    {
-      img: 'parking',
-      name: 'Free Parking',
-      money: 0,
-      human: false,
-      inGame: true,
-    },
-  ];
-
   playersUrl = '/api/players';
   httpOptions = {
     headers: new HttpHeaders({
@@ -83,9 +23,6 @@ export class PlayerService {
 
   getUrlBase(): string {
     return environment.apiUrlBase;
-  }
-  getPlayers(): Player[] {
-    return this.players;
   }
 
   /** GET heroes from the server */
@@ -169,14 +106,14 @@ export class PlayerService {
     player.inGame = false;
   }
 
-  reset() {
-    for (let p of this.players) {
-      p.inGame = true;
-      if (p.name == 'Free Parking') {
-        p.money = 0;
-      } else {
-        p.money = this.startingMoney;
-      }
-    }
-  }
+  // reset() {
+  //   for (let p of this.players) {
+  //     p.inGame = true;
+  //     if (p.name == 'Free Parking') {
+  //       p.money = 0;
+  //     } else {
+  //       p.money = this.startingMoney;
+  //     }
+  //   }
+  // }
 }
