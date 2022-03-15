d23743da358858014c43717f65cedf0b974ced66
waterproofpatch <waterproofpatch@gmail.com>
2022-03-15 02:03:08 -0400

    transaction updates trigger players updates

--- frontend/src/app/services/transaction.service.ts
+++ frontend/src/app/services/transaction.service.ts
@@ -5,6 +5,7 @@ import { BehaviorSubject } from 'rxjs';
 import { Transaction } from '../types';
 import { TransactionsApiService } from '../transactions-api.service';
 import { BaseComponent } from '../base/base/base.component';
+import { PlayerService } from './player.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,39 +13,43 @@ import { BaseComponent } from '../base/base/base.component';
 export class TransactionService extends BaseComponent {
   transactions$ = new BehaviorSubject<Transaction[]>([]);
 
-  constructor(private transactionsApi: TransactionsApiService) {
+  constructor(
+    private transactionsApi: TransactionsApiService,
+    private playerService: PlayerService
+  ) {
     super();
   }
 
-  private setTransactions(transactions: Transaction[]) {
+  private setTransactions(transactions: Transaction[], gameId: number) {
     this.transactions$.next(transactions);
+    this.playerService.getPlayersForGame(gameId);
   }
 
   addTransaction(transaction: Transaction, gameId: number) {
     this.transactionsApi
       .addTransactionHttp(transaction, gameId)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((x) => this.setTransactions(x));
+      .subscribe((x) => this.setTransactions(x, gameId));
   }
 
   getTransactionsForGame(gameId: number) {
     this.transactionsApi
       .getTransactionsHttp(gameId)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((x) => this.setTransactions(x));
+      .subscribe((x) => this.setTransactions(x, gameId));
   }
 
   updateTransactionsForGame(gameId: number) {
     this.transactionsApi
       .getTransactionsHttp(gameId)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((x) => this.setTransactions(x));
+      .subscribe((x) => this.setTransactions(x, gameId));
   }
 
-  deleteTransaction(transactionId: number) {
+  deleteTransaction(transaction: Transaction) {
     this.transactionsApi
-      .deleteTransactionHttp(transactionId)
+      .deleteTransactionHttp(transaction.ID)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((x) => this.setTransactions(x));
+      .subscribe((x) => this.setTransactions(x, transaction.GameID));
   }
 }
