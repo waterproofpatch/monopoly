c19b66fc3aed7fbc5ace638d9db98136b81367bb
waterproofpatch <waterproofpatch@gmail.com>
2022-03-26 14:57:22 -0400

    authenticated wrapper, better logging, update WriteError to take the status as an argument

--- backend/src/utils/logger.go
+++ backend/src/utils/logger.go
@@ -10,16 +10,34 @@ func Logger(inner func(http.ResponseWriter, *http.Request), name string) func(ht
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		log.Printf(
+			"Start %s - %s %s", r.RemoteAddr, r.Method, r.RequestURI)
+
 		inner(w, r)
 
+		timeTaken := time.Since(start)
 		log.Printf(
-			"%s%s\t%s\t%s\t%s%d",
-			r.RemoteAddr,
-			r.Method,
-			r.RequestURI,
-			r.Response.Status,
-			name,
-			time.Since(start),
-		)
+			"End %s - %s %s - took %d", r.RemoteAddr, r.Method, r.RequestURI, timeTaken)
+
+	}
+}
+
+func Authenticated(inner func(http.ResponseWriter, *http.Request), name string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		log.Printf(
+			"Start %s - %s %s", r.RemoteAddr, r.Method, r.RequestURI)
+
+		if !IsAuthorized(w, r) {
+			WriteError(w, "Request failed!", http.StatusUnauthorized)
+			return
+		}
+		inner(w, r)
+
+		timeTaken := time.Since(start)
+		log.Printf(
+			"End %s - %s %s - took %d", r.RemoteAddr, r.Method, r.RequestURI, timeTaken)
+
 	}
 }
