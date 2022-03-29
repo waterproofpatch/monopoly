45f857b43edcb01f55fde89b4fd0aa37d5b3e044
waterproofpatch <waterproofpatch@gmail.com>
2022-03-29 06:25:16 -0400

    redirect

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AuthenticationApiService } from '../apis/authentication-api.service';
 import { BaseService } from './base.service';
+import { Router } from '@angular/router';
+import { DialogService } from './dialog/dialog.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService extends BaseService {
   TOKEN_KEY = 'token';
-  constructor(private loginApi: AuthenticationApiService) {
+  constructor(
+    private loginApi: AuthenticationApiService,
+    private router: Router,
+    private dialogService: DialogService
+  ) {
     super();
   }
 
@@ -21,12 +27,15 @@ export class AuthenticationService extends BaseService {
 
   logout() {
     localStorage.removeItem(this.TOKEN_KEY);
+    this.dialogService.displayLogDialog('Logged out successfully.');
+    this.router.navigateByUrl('/');
   }
 
   register(email: string, password: string) {
     this.loginApi.registerHtp(email, password).subscribe((x) => {
       console.log('Setting token to ' + x.token);
       localStorage.setItem(this.TOKEN_KEY, x.token);
+      this.router.navigateByUrl('/');
     });
   }
 
@@ -34,6 +43,7 @@ export class AuthenticationService extends BaseService {
     this.loginApi.loginHttp(email, password).subscribe((x) => {
       console.log('Setting token to ' + x.token);
       localStorage.setItem(this.TOKEN_KEY, x.token);
+      this.router.navigateByUrl('/');
     });
   }
 }
