c407bcc1fe5d0f498ea4b7e7b17dcb9b366fabc0
waterproofpatch <waterproofpatch@gmail.com>
2022-03-29 06:07:31 -0400

    get games on authenticated

--- frontend/src/app/components/game-board/game-board.component.ts
+++ frontend/src/app/components/game-board/game-board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { GameService } from '../../services/game.service';
+import { AuthenticationService } from '../../services/authentication.service';
 import { BaseComponent } from '../base/base.component';
 
 @Component({
@@ -9,8 +10,14 @@ import { BaseComponent } from '../base/base.component';
   styleUrls: ['./game-board.component.css'],
 })
 export class GameBoardComponent extends BaseComponent implements OnInit {
-  constructor(public gameService: GameService) {
+  constructor(
+    public gameService: GameService,
+    private authenticationService: AuthenticationService
+  ) {
     super();
+    if (authenticationService.isAuthenticated) {
+      this.gameService.getGames();
+    }
   }
 
   ngOnInit(): void {}
