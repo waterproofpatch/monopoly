079e5473abe593dcb642512583f23636cfc1ab05
waterproofpatch <waterproofpatch@gmail.com>
2022-04-05 18:35:12 -0500

    push step

--- backend/heroku-deploy.sh
+++ backend/heroku-deploy.sh
@@ -7,5 +7,6 @@ heroku container:login
 #heroku ps:scale web=1 --app monopoly-banker-backend
 docker tag monopoly-backend registry.heroku.com/monopoly-banker-backend/web
 docker push registry.heroku.com/monopoly-banker-backend/web
+heroku container:push web --app monopoly-banker-backend
 heroku container:release web --app monopoly-banker-backend
 heroku ps:scale web=1 --app monopoly-banker-backend
