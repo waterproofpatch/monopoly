1f5f423669df0f0e7cf87af6f2dd3d5eeb37a5ba
waterproofpatch <waterproofpatch@gmail.com>
2022-04-05 18:35:35 -0500

    refactor

--- backend/heroku-deploy.sh
+++ backend/heroku-deploy.sh
@@ -1,10 +1,7 @@
 # this must berun within the context of the directory containing the Dockerfile.
-heroku container:login
 # 'web' is the process type
 # 'monopoly-banker-backend' is the name of the heroku application
-#heroku container:push web --app monopoly-banker-backend
-#heroku container:release web --app monopoly-banker-backend
-#heroku ps:scale web=1 --app monopoly-banker-backend
+heroku container:login
 docker tag monopoly-backend registry.heroku.com/monopoly-banker-backend/web
 docker push registry.heroku.com/monopoly-banker-backend/web
 heroku container:push web --app monopoly-banker-backend
